b75c11548596958aa2163439c00de947d3ba2d54
Dhiraj Wishal <wishaldhiraj@gmail.com>
2022-02-10 17:21:21 +0530

    Edited the build system and the gitignore file.

--- Desktop/RealityCore/RealityCore.pro
+++ Desktop/RealityCore/RealityCore.pro
@@ -2,13 +2,11 @@ QT       += core gui
 
 greaterThan(QT_MAJOR_VERSION, 5): QT += widgets
 
-CONFIG += \
-    c++2a \
+CONFIG +=                                       \
+    c++2a                                       \
     file_copies
 
-# You can make your code fail to compile if it uses deprecated APIs.
-# In order to do so, uncomment the following line.
-#DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
+DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
 SOURCES +=                                      \
     Source/Main.cpp                             \
--- Scripts/Build.py
+++ Scripts/Build.py
@@ -41,41 +41,20 @@ def create_directory(path: str) -> None:
 		pass
 
 
-def build_desktop_scipper() -> None:
+def build_desktop() -> None:
 	"""
-	Build the decktop scipper (screen capturing API) library.
+	Build the desktop application.
 
 	:return: None
 	"""
 
-
 	# Build the screen capture lite library.
-	os.system("cd \"Desktop/Scipper/ThirdParty/screen_capture_lite\" && cmake CMakeLists.txt && cmake --build . --config Release --clean-first")
-	os.system("cd \"Desktop/Scipper/ThirdParty/screen_capture_lite\" && cmake CMakeLists.txt && cmake --build . --config Debug --clean-first")
-
-	# Make the required build directories (they're made automatically by the compiler but we need it explicitly here because we need to copy paste the 
-	# third party binaries there beforehand).
-	create_directory("Desktop/Scipper/Builds/Debug")
-	create_directory("Desktop/Scipper/Builds/Release")
-
-	# Build premake and the project files.
-	if is_on_windows():
-		# Copy the screen capture lite dlls to the correct folders.
-		shutil.copy("Desktop/Scipper/ThirdParty/screen_capture_lite/Debug/screen_capture_lite_shared.dll", "Desktop/Scipper/Builds/Debug")
-		shutil.copy("Desktop/Scipper/ThirdParty/screen_capture_lite/Release/screen_capture_lite_shared.dll", "Desktop/Scipper/Builds/Release")
-
-		os.system("cd \"Desktop/Scipper\" && call \"Tools/Windows/premake5\" vs2022")
-
-	else:
-		# Copy the screen capture lite dlls to the correct folders.
-		shutil.copy("\"Desktop/Scipper/ThirdParty/screen_capture_lite/Debug/screen_capture_lite_shared.so\"", "\"Desktop/Scipper/Builds/Debug\"")
-		shutil.copy("\"Desktop/Scipper/ThirdParty/screen_capture_lite/Release/screen_capture_lite_shared.so\"", "\"Desktop/Scipper/Builds/Release\"")
-
-		os.system("cd \"Desktop/Scipper\" && call \"Tools/Linux/premake5\" gmake2")
+	os.system("cd \"Desktop/RealityCore/ThirdParty/screen_capture_lite\" && cmake CMakeLists.txt && cmake --build . --config Release --clean-first")
+	os.system("cd \"Desktop/RealityCore/ThirdParty/screen_capture_lite\" && cmake CMakeLists.txt && cmake --build . --config Debug --clean-first")
 
 
 if __name__ == "__main__":
 	print("Reality Core build script.")
 
-	print_statement("Building Scipper...")
-	build_desktop_scipper()
+	print_statement("Building Desktop...")
+	build_desktop()
